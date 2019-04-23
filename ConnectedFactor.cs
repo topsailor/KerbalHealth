39dca7338ba3e85dd73e69a5627ef414a8fe2ff6
JWSim <topsailor@gmail.com>
2019-04-23 15:17:56 +0900

    Added Primitive Support for RemoteTech

--- ConnectedFactor.cs
+++ ConnectedFactor.cs
@@ -9,7 +9,16 @@
         public override double ChangePerDay(ProtoCrewMember pcm)
         {
             if (Core.IsInEditor) return IsEnabledInEditor() ? BaseChangePerDay : 0;
-            return (Core.IsKerbalLoaded(pcm) && (Core.KerbalVessel(pcm).Connection != null) && Core.KerbalVessel(pcm).Connection.IsConnectedHome) ? BaseChangePerDay : 0;
+            if (RemoteTech.API.API.IsRemoteTechEnabled())
+            {
+                return (Core.IsKerbalLoaded(pcm) && (RemoteTech.API.API.HasConnectionToKSC(Core.KerbalVessel(pcm).id))) ? BaseChangePerDay : 0;
+
+            }
+            else
+            {
+                return (Core.IsKerbalLoaded(pcm) && (Core.KerbalVessel(pcm).Connection != null) && Core.KerbalVessel(pcm).Connection.IsConnectedHome) ? BaseChangePerDay : 0;
+
+            }
         }
     }
 }
